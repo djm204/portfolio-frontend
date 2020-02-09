839e98dd0c0260b3a173f98b32e4679dbf9cfd35
David Mendez <desiratech@gmail.com>
2020-02-09 17:18:10 -0600

    homepage data and function complete

--- src/pages/TechSummary/TechSummary.tsx
+++ src/pages/TechSummary/TechSummary.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TechSummary = () => {
     return (
         <React.Fragment>
             TechSummary
+            <Link to='/contact'>Contact Me</Link>
         </React.Fragment>
     );
 }
