a6270f8f09cca11eb175cd5dfb326023985b21a6
David Mendez <desiratech@gmail.com>
2020-02-11 23:22:32 -0600

    projects section completed

--- src/pages/TechSummary/TechSummary.tsx
+++ src/pages/TechSummary/TechSummary.tsx
@@ -1,114 +1,12 @@
 import React from 'react';
 import { InfoSection } from '../../components/layout/InfoSection';
-import Project from '../../components/Project';
+import Project from './Project';
 import StatsBar from './StatsBar';
 import '../../stylesheets/TechSummary.css'
+import { skills, projects } from './dataSource';
 
 
 const TechSummary = () => {
-    const projects = [
-        {
-            className: 'Projects_Cashier',
-            name: 'Cashier',
-            role: 'Developer Team Lead / Full Stack Developer',
-            points: [
-                'Responsible for pair programming / mentoring and unblocking reports.',
-                'Delegation and prioritization of tasks with a workload of about 25-45 tickets per week.',
-                'React/Redux App on frontend for Admin and Checkout.',
-                'Hybrid PHP / Go backend.',
-                '4 years on the team working to deliver features and bug fixes for both the frontend and backend.'
-            ],
-            dataAos: 'fade-right',
-            dataAosDuration: '1000',
-            dataAosEasing: 'ease-in-out',
-            dataAosOnce: 'true'
-        },
-        {
-            className: 'Projects_Staples',
-            name: 'Staples Project',
-            role: 'Developer Team Lead / Full Stack Developer',
-            points: [
-                'React/Redux App.',
-                'Customized the current functionality of Cashier to allow for Staples business requirements to be met.',
-                'Successful delivery on tight timeline.'
-            ],
-            dataAos: 'fade-up',
-            dataAosDuration: '1000',
-            dataAosEasing: 'ease-in-out',
-            dataAosOnce: 'true'
-        },
-        {
-            className: 'Projects_MultiCurrency',
-            name: 'Bold MultiCurrency',
-            role: 'Completed the first iteration of the frontend of the app in its entirety.',
-            points: [
-                'React/Redux App converting prices in a Shopify / BigCommerce store, by currency and ruleset define by the merchant.',
-                'Customized the current functionality of Cashier to allow for Staples business requirements to be met.',
-                'Helped write the integration for Cashier to allow for charging in specific currencies.'
-            ],
-            dataAos: 'fade-up',
-            dataAosDuration: '1000',
-            dataAosEasing: 'ease-in-out',
-            dataAosOnce: 'true'
-        },
-        {
-            className: 'Projects_DeepRoots',
-            name: 'Deep Roots Strong Dance - Website',
-            role: 'Designed and built entire site',
-            points: [
-                'React/Redux App.',
-                'API endpoint for adding to mailing list.',
-            ],
-            dataAos: 'fade-left',
-            dataAosDuration: '1000',
-            dataAosEasing: 'ease-in-out',
-            dataAosOnce: 'true'
-        }
-    ];
-
-    const skills = [
-        {
-            name: 'HTML',
-            percentage: 95
-        },
-        {
-            name: 'CSS',
-            percentage: 90
-        },
-        {
-            name: 'JavaScript',
-            percentage: 90
-        },
-        {
-            name: 'React',
-            percentage: 90
-        },
-        {
-            name: 'Redux',
-            percentage: 85
-        },
-        {
-            name: 'TypeScript',
-            percentage: 75
-        },
-        {
-            name: 'NodeJS',
-            percentage: 65
-        },
-        {
-            name: 'Go',
-            percentage: 65
-        },
-        {
-            name: 'K8s',
-            percentage: 55
-        },
-        {
-            name: 'GCP',
-            percentage: 55
-        },
-    ];
-
     return (
         <div className="TechSummaryPage">
             <InfoSection
@@ -117,7 +15,7 @@ const TechSummary = () => {
                 data-aos-easing="ease-in-out"
                 data-aos-once="true"
                 classnames="TechSummaryPage__InfoSection"
-                >
+            >
                 <div className="TechSummaryPage__Introduction">
                     <h2>A Brief History</h2>
                     <p>David has been intrigued with Technology from a very small age. He was first introduced by his parents via a IBM PS/2 486 Desktop PC.</p>
@@ -132,7 +30,9 @@ const TechSummary = () => {
             </InfoSection>
             <InfoSection classnames="TechSummaryPage__Projects">
                 <h2>Projects</h2>
-                {projects.map(project => <Project key={JSON.stringify(project)} {...project} />)}
+                <div className="TechSummary__Projects__Wrapper">
+                    {projects.map(project => <Project key={JSON.stringify(project)} {...project} />)}
+                </div>
             </InfoSection>
         </div>
     );
