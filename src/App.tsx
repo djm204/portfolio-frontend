cc99d89de1a9d7607690a0d0aed5dd93922a015f
David Mendez <desiratech@gmail.com>
2020-02-12 21:58:16 -0600

    update some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import React from 'react';
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import About from './pages/About/About';
-import TechSummary from './pages/TechSummary/TechSummary';
-import Contact from './pages/Contact/Contact';
-import Navigation from './components/Navigation';
-import HorizontalSeparator from './components/layout/HorizontalSeparator';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebookSquare, faInstagramSquare, faTwitterSquare, faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
-import { faArrowAltDown, faEnvelopeSquare, faChevronRight, faEgg } from '@fortawesome/pro-solid-svg-icons'
+import React from 'react'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import About from './pages/About/About'
+import TechSummary from './pages/TechSummary/TechSummary'
+import Contact from './pages/Contact/Contact'
+import Navigation from './components/Navigation'
+import HorizontalSeparator from './components/layout/HorizontalSeparator'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+  faTwitterSquare,
+  faLinkedin,
+  faGithubSquare,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faArrowAltDown,
+  faEnvelopeSquare,
+  faChevronRight,
+  faEgg,
+} from '@fortawesome/pro-solid-svg-icons'
 
-AOS.init();
+AOS.init()
 
-library.add( faArrowAltDown, faEgg, faChevronRight, faFacebookSquare, faInstagramSquare, faTwitterSquare, faLinkedin, faGithubSquare, faEnvelopeSquare );
+library.add(
+  faArrowAltDown,
+  faEgg,
+  faChevronRight,
+  faFacebookSquare,
+  faInstagramSquare,
+  faTwitterSquare,
+  faLinkedin,
+  faGithubSquare,
+  faEnvelopeSquare
+)
 
 const App = () => {
   return (
-      <div id="App__Container">
+    <div id="App__Container">
       <Header />
       <Navigation />
-          <About /> 
-          <HorizontalSeparator classnames={['lighter', 'left-to-right']} />
-          <TechSummary />
-          <HorizontalSeparator classnames={['darker', 'left-to-right']} />
-          <Contact />
-          <Footer />
-      </div>
-  );
+      <About />
+      <HorizontalSeparator classnames={['lighter', 'left-to-right']} />
+      <TechSummary />
+      <HorizontalSeparator classnames={['darker', 'left-to-right']} />
+      <Contact />
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,46 +1,53 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react'
 
-import { InfoSection } from '../../components/layout/InfoSection';
-import Image from '../../components/Image';
-import Bio from './Bio';
-import '../../stylesheets/About.css';
+import { InfoSection } from '../../components/layout/InfoSection'
+import Image from '../../components/Image'
+import Bio from './Bio'
+import '../../stylesheets/About.css'
 
 const Home: FunctionComponent = () => {
-    const bioPicUrl = 'david_on_a_mountain.jpg';
+  const bioPicUrl = 'david_on_a_mountain.jpg'
 
-    return (
-        <div
-            id="About" 
-            className="About"
-            data-aos="fade-in"
-            data-aos-easing="ease-in-out"
-            data-aos-once="true"
-            data-aos-duration="1000">
-            <InfoSection classnames="Bio">
-                <div className="Bio__Image">
-                    <Image
-                        imgClasses="Bio__Image__Picture"
-                        imgUrl={bioPicUrl || ''}
-                        altText="David Mendez" />
-                    <h4>David Mendez</h4>
-                    <h5>Developer</h5>
-                </div>
-                <div className="Bio__AndQuote">
-                    <Bio>
-                        <p>David is a life-long techy with an insatiable appetite for software development.</p>
-                        <p>He spends much of his free time in front of the PC creating and furthering his knowledge about different technologies such as JavaScript, GoLang and Python.</p>
-                        <p>Staying up to date with technology, code coverage and best practices are very important to David.</p>
-                    </Bio>
-                    <div className="Quote">
-                        <h4>Favourite Quote</h4>
-                        <blockquote>
-                            "Why settle for normalcy when there's a whole world of weird to explore?"
-                        </blockquote>
-                    </div>
-                </div>
-            </InfoSection>
+  return (
+    <div
+      id="About"
+      className="About"
+      data-aos="fade-in"
+      data-aos-easing="ease-in-out"
+      data-aos-once="true"
+      data-aos-duration="1000"
+    >
+      <InfoSection classnames="Bio">
+        <div className="Bio__Image">
+          <Image imgClasses="Bio__Image__Picture" imgUrl={bioPicUrl || ''} altText="David Mendez" />
+          <h4>David Mendez</h4>
+          <h5>Developer</h5>
         </div>
-    );
+        <div className="Bio__AndQuote">
+          <Bio>
+            <p>
+              David is a life-long technology enthusiast with an insatiable appetite for software
+              development.
+            </p>
+            <p>
+              He spends much of his free time in front of the PC creating and furthering his
+              knowledge about different technologies such as JavaScript, GoLang and Python.
+            </p>
+            <p>
+              Staying up to date with technology, code coverage and best practices are very
+              important to David.
+            </p>
+          </Bio>
+          <div className="Quote">
+            <h4>Favourite Quote</h4>
+            <blockquote>
+              "Why settle for normalcy when there's a whole world of weird to explore?"
+            </blockquote>
+          </div>
+        </div>
+      </InfoSection>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
