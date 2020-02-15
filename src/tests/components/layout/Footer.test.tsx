35e961fd9966ebf2297c8b95621177930f7e7cfa
David Mendez <desiratech@gmail.com>
2020-02-15 16:29:10 -0600

    fix tests

--- src/tests/components/layout/Footer.test.tsx
+++ src/tests/components/layout/Footer.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Footer from '../../../components/layout/Footer'
 import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 it('Footer renders without crashing', () => {
+  const fetchSpy = jest.spyOn(window, 'fetch')
+
   render(<Footer />)
+  expect(fetchSpy).toBeCalled()
 })
--- src/tests/pages/Contact/Contact.test.tsx
+++ src/tests/pages/Contact/Contact.test.tsx
@@ -1,11 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Contact from '../../../pages/Contact/Contact'
 import { render } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 
 it('Contact renders without crashing', async () => {
-  const fetchSpy = jest.spyOn(window, 'fetch')
-
   render(<Contact />)
-  expect(fetchSpy).toBeCalled()
 })
