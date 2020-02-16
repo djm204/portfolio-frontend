31d5c0024bf1cd201136c1e528ef23c4e9ddbde9
David Mendez <desiratech@gmail.com>
2020-02-16 17:13:09 -0600

    Egg styling completed

--- src/components/Egg.tsx
+++ src/components/Egg.tsx
@@ -8,24 +8,24 @@ const Egg = () => {
   const [alreadyShown, setAlreadyShown] = useState([])
 
   const showEgg = () => {
-    if (alreadyShown.length === 4) {
+    const eggClassToSelect = selectEggClassAtRandom(alreadyShown)
+    alreadyShown.push(eggClassToSelect as never)
+    setAlreadyShown(alreadyShown)
+
+    let eggElement = document.querySelector(eggClassToSelect)
+    setShowFriedEgg(true)
+
+    eggElement?.classList.remove('Egg__Hide')
+    eggElement?.classList.add('Egg__Show')
+
+    setTimeout(() => {
+      eggElement?.classList.add('Egg__Hide')
+      eggElement?.classList.remove('Egg__Show')
+      setShowFriedEgg(false)
+    }, 3000)
+
+    if (alreadyShown.length >= 3) {
       setAlreadyShown([])
-    } else {
-      const eggClassToSelect = selectEggClassAtRandom(alreadyShown)
-      alreadyShown.push(eggClassToSelect as never)
-      setAlreadyShown(alreadyShown)
-
-      let eggElement = document.querySelector(eggClassToSelect)
-      setShowFriedEgg(true)
-
-      eggElement?.classList.remove('Egg__Hide')
-      eggElement?.classList.add('Egg__Show')
-
-      setTimeout(() => {
-        eggElement?.classList.add('Egg__Hide')
-        eggElement?.classList.remove('Egg__Show')
-        setShowFriedEgg(false)
-      }, 3000)
     }
   }
 
@@ -33,22 +33,22 @@ const Egg = () => {
 
   return (
     <React.Fragment>
-      <div className="EggBert" onClick={showEgg}>
+      <button className="EggBert" onClick={showEgg} disabled={showFriedEgg}>
         <FontAwesomeIcon icon={iconClass} />
-      </div>
-      <div className="Egg--1 Egg__Hide">
+      </button>
+      <div className="Egg Egg--1 Egg__Hide">
         <Image
           imgUrl="https://davidmendez.dev/i_love_to_festival.jpg"
           altText="I love to festival"
         />
       </div>
-      <div className="Egg--2 Egg__Hide">
+      <div className="Egg Egg--2 Egg__Hide">
         <Image imgUrl="https://davidmendez.dev/i_love_to_garden.jpg" altText="I love to garden" />
       </div>
-      <div className="Egg--3 Egg__Hide">
+      <div className="Egg Egg--3 Egg__Hide">
         <Image imgUrl="https://davidmendez.dev/i_love_my_cats.jpg" altText="I love my cats" />
       </div>
-      <div className="Egg--4 Egg__Hide">
+      <div className="Egg Egg--4 Egg__Hide">
         <Image imgUrl="https://davidmendez.dev/me_and_my_love.jpg" altText="My love and I" />
       </div>
     </React.Fragment>
